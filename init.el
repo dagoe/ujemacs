92ca0bd3b94f5ae3aa85f939c3c6c8a430d32803
Uwe Jaekel <uwe.jaekel@gmail.com>
2022-01-29 21:01:07 +0100

    test.org and .gitignore added

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 
 (setq inhibit-startup-message t)
 
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -20,12 +21,25 @@
   :ensure t
   :config (which-key-mode))
 
+;; Org-mode stuff
+;; --------------
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook #'org-bullets-mode))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (R . t)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages '(org-bullets which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
